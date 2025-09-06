42aaea5f40b1637c4b47c7f1e0b81da390d1586f
xenonbytesarl <xenonbytesarl@gmail.com>
2025-09-06 16:03:47 +0200

    feature/0002: Fix page and size change in paginator

--- frontend/src/app/features/product-categories/screens/product-categories-list.ts
+++ frontend/src/app/features/product-categories/screens/product-categories-list.ts
@@ -11,6 +11,7 @@ import { PaginatorComponent } from '../../../shared/ui/paginator';
 import { SpinnerComponent } from '../../../shared/ui/spinner';
 import {ConfirmDialogService} from '../../../shared/ui/confirm-dialog';
 import {TranslateService} from '../../../core/i18n/translate.service';
+import {Direction} from '../../../core/model/direction.enum';
 
 @Component({
   selector: 'app-product-categories-list-page',
@@ -33,7 +34,13 @@ import {TranslateService} from '../../../core/i18n/translate.service';
         </div>
 
         <app-product-category-list [items]="store.categoryPage().elements" (edit)="goEdit($event)" (remove)="remove($event)" />
-        <app-paginator [total]="store.categoryPage().totalElements" [(page)]="store.categoryPage().page" [(pageSize)]="store.categoryPage().size" />
+        <app-paginator
+          [total]="store.categoryPage().totalElements"
+          [page]="uiPage()"
+          [pageSize]="uiPageSize()"
+          (pageChange)="onPageChange($event)"
+          (pageSizeChange)="onPageSizeChange($event)"
+        />
       </app-card>
     </div>
   `,
@@ -49,8 +56,22 @@ export class ProductCategoriesListPage {
 
   loading = computed(() => this.store.loading());
 
+  // UI page is 1-based; backend/store page is 0-based
+  uiPage = computed(() => (this.store.categoryPage().page ?? 0) + 1);
+  uiPageSize = computed(() => this.store.categoryPage().size ?? 10);
+
   constructor() {
     this.route.data.subscribe(data => {});
+  }
+
+  async onPageChange(page1Based: number) {
+    const size = this.uiPageSize();
+    await this.store.search('', page1Based - 1, size, Direction.ASC, 'name');
+  }
+
+  async onPageSizeChange(size: number) {
+    // Reset page to 0 when size changes
+    await this.store.search('', 0, size, Direction.ASC, 'name');
   }
 
   goEdit(id: string) {
