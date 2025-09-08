36eec5ed079e7c703bcf5714c3b6ca1adcd3cb93
xenonbytesarl <xenonbytesarl@gmail.com>
2025-09-08 20:55:05 +0200

    feature/0003:: create actors resolvers. Closes #11

--- frontend/src/app/features/actors/routes.ts
+++ frontend/src/app/features/actors/routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import {actorFindByIdResolver, actorSearchResolver} from './actor.resolver';
 
 export const actorsRoutes: Routes = [
   {
@@ -6,10 +7,25 @@ export const actorsRoutes: Routes = [
     loadComponent: () => import('./page').then(m => m.ActorsPage),
     children: [
       { path: '', pathMatch: 'full', redirectTo: 'list' },
-      { path: 'list', loadComponent: () => import('./screens/actors-list').then(m => m.ActorsListPage) },
+      {
+        path: 'list', loadComponent: () => import('./screens/actors-list').then(m => m.ActorsListPage),
+        resolve: {
+          actorsSearch: actorSearchResolver
+        }
+      },
       { path: 'new', loadComponent: () => import('./screens/actor-new').then(m => m.ActorNewPage) },
-      { path: ':id', loadComponent: () => import('./screens/actor-view').then(m => m.ActorViewPage) },
-      { path: ':id/edit', loadComponent: () => import('./screens/actor-edit').then(m => m.ActorEditPage) },
+      {
+        path: ':id', loadComponent: () => import('./screens/actor-view').then(m => m.ActorViewPage),
+        resolve: {
+          actorFindById: actorFindByIdResolver,
+        }
+      },
+      {
+        path: ':id/edit', loadComponent: () => import('./screens/actor-edit').then(m => m.ActorEditPage),
+        resolve: {
+          actorFindById: actorFindByIdResolver,
+        }
+      },
     ]
   }
 ];
