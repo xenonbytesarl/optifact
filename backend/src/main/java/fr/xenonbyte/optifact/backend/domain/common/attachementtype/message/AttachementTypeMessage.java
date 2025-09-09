ce3898f0c2b19021d7f69e39014851db6f7d369f
xenonbytesarl <xenonbytesarl@gmail.com>
2025-09-09 23:10:44 +0200

    feature/0004:: Add attachment in domain layer

---
+++ backend/src/main/java/fr/xenonbyte/optifact/backend/domain/common/attachementtype/message/AttachementTypeMessage.java
@@ -0,0 +1,12 @@
+package fr.xenonbyte.optifact.backend.domain.common.attachementtype.message;
+
+/**
+ * @author bamk
+ * @version 1.0
+ * @since 09/09/2025
+ */
+public final class AttachementTypeMessage {
+    public static final String ATTACHMENT_TYPE_NAME_REQUIRED = "attachment.type.name.required";
+    public static final String ATTACHMENT_TYPE_NAME_CONFLICT = "attachment.type.name.conflict";
+    public static final String ATTACHMENT_TYPE_ID_NOT_FOUND = "attachment.type.id.not.found";
+}
